8d0bf3dac8524e1c3cfdabee53de54e7838e3074
Cristopher.quiroz <Cristopher.quiroz@telemedik.com>
2024-10-19 01:03:06 -0400

    fix delete new item

--- src/App.jsx
+++ src/App.jsx
@@ -62,14 +62,21 @@ export default function App() {
       console.error('Error durante la confirmación:', error);
     } 
   };
+  function generateGUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0,
+          v = c === 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
 
   const handleAddProcedure =  () => {
      setEditingProcedure([
       ...editingProcedure, 
       {
-        __typename
+        id
         : 
-        "Procedimiento"} 
+        generateGUID()} 
     ])
   }
 
